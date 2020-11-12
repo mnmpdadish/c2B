32fdc16acddfeced8642c5d8a3b2087fc11220ee
Maxime Charlebois <mnmpdadish@hotmail.com>
2020-11-12 17:05:54 -0500

    BUG SOLVED!

--- src/model.h
+++ src/model.h
@@ -47,6 +47,7 @@ public:
         
     //matrices
     BasicMatrix tc;
+    BasicMatrix tc0;
     BasicMatrix dtk;
     BasicMatrix tc2;
     BasicMatrix dtk2;
@@ -64,7 +65,7 @@ public:
     
     Model():
     
-    tc(4), dtk(4), tc2(4), dtk2(4), dtk3(4), dtk4(4), green(4), cumul(4), sigma(4), delta(4), deltak(4)
+    tc(4), tc0(4), dtk(4), tc2(4), dtk2(4), dtk3(4), dtk4(4), green(4), cumul(4), sigma(4), delta(4), deltak(4)
 
     {
         if (not exists("para.dat")) {printf("ERROR: couldn't find file 'para.dat'\n\n"); exit(1);}
@@ -103,15 +104,8 @@ public:
         //if(dbEqual(D,0.0)) printf("is not superconducter\n");
         //if(dbEqual(M,0.0)) printf("is not AFM\n");
 
-        tc(0,0)=  0.; tc(0,1)= -t;  tc(0,2)= -tp; tc(0,3)= -t;
-        tc(1,0)= -t;  tc(1,1)=  0.; tc(1,2)= -t;  tc(1,3)= -tp;
-        tc(2,0)= -tp; tc(2,1)= -t;  tc(2,2)=  0.; tc(2,3)= -t;
-        tc(3,0)= -t;  tc(3,1)= -tp; tc(3,2)= -t;  tc(3,3)=  0.;
 
-        tc2(0,0)=  0.; tc2(0,1)=  t;  tc2(0,2)= -tp; tc2(0,3)=  t;
-        tc2(1,0)=  t;  tc2(1,1)=  0.; tc2(1,2)=  t;  tc2(1,3)= -tp;
-        tc2(2,0)= -tp; tc2(2,1)=  t;  tc2(2,2)=  0.; tc2(2,3)=  t;
-        tc2(3,0)=  t;  tc2(3,1)= -tp; tc2(3,2)=  t;  tc2(3,3)=  0.;
+
 
     }
     
@@ -131,6 +125,22 @@ public:
     void calculate_dtk(const double kx, const double ky)
     //Hk = tk matrix
     {
+
+        tc(0,0)=  0.; tc(0,1)= -t;  tc(0,2)= -tp; tc(0,3)= -t;
+        tc(1,0)= -t;  tc(1,1)=  0.; tc(1,2)= -t;  tc(1,3)= -tp;
+        tc(2,0)= -tp; tc(2,1)= -t;  tc(2,2)=  0.; tc(2,3)= -t;
+        tc(3,0)= -t;  tc(3,1)= -tp; tc(3,2)= -t;  tc(3,3)=  0.;
+
+        tc2(0,0)=  0.; tc2(0,1)=  t;  tc2(0,2)= -tp; tc2(0,3)=  t;
+        tc2(1,0)=  t;  tc2(1,1)=  0.; tc2(1,2)=  t;  tc2(1,3)= -tp;
+        tc2(2,0)= -tp; tc2(2,1)=  t;  tc2(2,2)=  0.; tc2(2,3)=  t;
+        tc2(3,0)=  t;  tc2(3,1)= -tp; tc2(3,2)=  t;  tc2(3,3)=  0.;
+
+        tc0(0,0)=  0.; tc0(0,1)= -t;  tc0(0,2)=  0.; tc0(0,3)= -t;
+        tc0(1,0)= -t;  tc0(1,1)=  0.; tc0(1,2)= -t;  tc0(1,3)=  0.;
+        tc0(2,0)=  0.; tc0(2,1)= -t;  tc0(2,2)=  0.; tc0(2,3)= -t;
+        tc0(3,0)= -t;  tc0(3,1)=  0.; tc0(3,2)= -t;  tc0(3,3)=  0.;
+
         complex<double> ex(cos(-kx*2.), sin(-kx*2.));
         complex<double> emx = conj(ex);
         complex<double> ey(cos(-ky*2.), sin(-ky*2.));
@@ -144,28 +154,12 @@ public:
 
         if(model==1){//this is for YRZ
           //assignation of the 4 by 4 Hk tk:
-          dtk4(0,0)=0;                          dtk(0,1)=-t*ex;                    dtk(0,2)=0;                      dtk(0,3)=-t*ey;
-          dtk4(1,0)=-t*emx;                     dtk(1,1)=0;                        dtk(1,2)=-t*ey;                  dtk(1,3)=0;
-          dtk4(2,0)=0;                          dtk(2,1)=-t*emy;                   dtk(2,2)=0;                      dtk(2,3)=-t*emx;
-          dtk4(3,0)=-t*emy;                     dtk(3,1)=0;                        dtk(3,2)=-t*ex;                  dtk(3,3)=0;
+          dtk4(0,0)=0;                          dtk4(0,1)=-t*ex;                    dtk4(0,2)=0;                      dtk4(0,3)=-t*ey;
+          dtk4(1,0)=-t*emx;                     dtk4(1,1)=0;                        dtk4(1,2)=-t*ey;                  dtk4(1,3)=0;
+          dtk4(2,0)=0;                          dtk4(2,1)=-t*emy;                   dtk4(2,2)=0;                      dtk4(2,3)=-t*emx;
+          dtk4(3,0)=-t*emy;                     dtk4(3,1)=0;                        dtk4(3,2)=-t*ex;                  dtk4(3,3)=0;
 
-          if (periodization>=2){
-            //this is for YRZ
-            deltak(0,0)=  M; deltak(0,1)=  0.; deltak(0,2)= 0.; deltak(0,3)= 0.;
-            deltak(1,0)= 0.; deltak(1,1)=  -M; deltak(1,2)= 0.; deltak(1,3)= 0.;
-            deltak(2,0)= 0.; deltak(2,1)=  0.; deltak(2,2)=  M; deltak(2,3)= 0.;
-            deltak(3,0)= 0.; deltak(3,1)=  0.; deltak(3,2)= 0.; deltak(3,3)= -M;
-          
-          }
-          else{
-            //this is for YRZ
-            delta(0,0)=  M; delta(0,1)=  0.; delta(0,2)= 0.; delta(0,3)= 0.;
-            delta(1,0)= 0.; delta(1,1)=  -M; delta(1,2)= 0.; delta(1,3)= 0.;
-            delta(2,0)= 0.; delta(2,1)=  0.; delta(2,2)=  M; delta(2,3)= 0.;
-            delta(3,0)= 0.; delta(3,1)=  0.; delta(3,2)= 0.; delta(3,3)= -M;
-          }
-
-         /* if (periodization>=2){
+         if (periodization>=2){
             deltak(0,0)= 0.;          deltak(0,1)=  M*(1.+ex);   deltak(0,2)= 0.;          deltak(0,3)= -M*(1.+ey);
             deltak(1,0)=  M*(1.+emx); deltak(1,1)= 0.;           deltak(1,2)= -M*(1.+ey);  deltak(1,3)= 0.;
             deltak(2,0)= 0.;          deltak(2,1)= -M*(1.+emy);  deltak(2,2)= 0.;          deltak(2,3)=  M*(1.+emx);
@@ -178,7 +172,7 @@ public:
             delta(1,0)=  M; delta(1,1)= 0.; delta(1,2)= -M; delta(1,3)= 0.;
             delta(2,0)= 0.; delta(2,1)= -M; delta(2,2)= 0.; delta(2,3)=  M;
             delta(3,0)= -M; delta(3,1)= 0.; delta(3,2)=  M; delta(3,3)= 0.;
-          }*/
+          }
         }
         else{
           // e^(i*pi) = -1;
@@ -226,7 +220,7 @@ public:
         for(int i=0;i<sigma.dim;i++)
             for(int j=0;j<sigma.dim;j++)
             {
-                sigma(i,j) = tc(i,j);
+                sigma(i,j) = tc0(i,j);
                 if (i==j) sigma(i,j) += z;
                 if (periodization>=2) sigma(i,j) += dtk4(i,j);
             }
@@ -268,18 +262,6 @@ public:
                 if(i==j) green(i,j) += z + MU;
             }
         green.invert();
-    }
-
-
-    void calculate_Gk(const double px, const double py)
-    {
-        complex<double> z(OMEGA,ETA);
-        calculate_dtk(px, py);
-        if(model==0)
-          calculate_sigma(z); // could be omitted when z doesn't change
-        else
-          calculate_sigmaYRZ(z);
-        calculate_Gk(px, py, z);
     }
 
 
